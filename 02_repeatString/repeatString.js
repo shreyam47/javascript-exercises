c9bdc00ce08d3be11b54d1616d97fba019a1913b
shreya <shreyamadhavan47@gmail.com>
2025-01-15 21:20:04 -0800

    solved exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let result = ""
+    if (num < 0) {
+        result = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            result += word;
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let result = ""
+    for (let i = phrase.length; i >= 0; i--) {
+        result += phrase.charAt(i)
+    }
+    return result
 };
 
 // Do not edit below this line
